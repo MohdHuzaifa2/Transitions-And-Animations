d28ab0d24ddcf046df1fb13b5fe4a37310a51500
Mohammad Huzaifa Ansari <107480387+MohdHuzaifa2@users.noreply.github.com>
2024-06-26 22:11:01 +0530

    Update script.js

--- part-4/script.js
+++ part-4/script.js
@@ -50,3 +50,16 @@ gsap.from(".page2 .box", {
         scrub: 2,
     }
 })
+gsap.from(".page3 .box", {
+    scale: 0,
+    duration: 1,
+    rotate: 360,
+    scrollTrigger: {
+        trigger: ".page3 .box",
+        scroller: "main",
+        // markers: true,
+        start: "top 60%",
+        end: "top 30%",
+        scrub: 2,
+    }
+})
